bcfca762561a336d7162bf94e418c264e67c6f66
Mabeamer <Mabeamer01@gmail.com>
2023-10-29 21:15:24 -0400

    upload

---
+++ main.py
@@ -0,0 +1,79 @@
+import csv
+import sys
+
+#cv search function
+
+
+reader = csv.reader(open('us-area-code-cities.csv', encoding="utf8"))
+
+#display what line the user should search frm (area code example, city, state, long/lat)
+#once selected have them search
+#display all found references to file
+#passing in placeholder***
+def Search():
+    ticker = 0
+    numCheck = False
+    #ask user to load file, will be preloading us-area-code-cities
+    #print("Please load csv file for editing")
+
+    print("Please select the column you wish to search from (Row number starts from 0)")
+    #print(reader[1])
+    for row in reader:
+        #TODO: refactor this could be bool 
+        if ticker < 1:
+            print(row)
+            #user makes choice here, need to find out how to keep them in bounds so they don't throw an exception
+            rowInput = int(input("Row Selection: "))
+
+            while numCheck == False:
+                try:
+                    print(row[rowInput])
+                    x = rowInput
+                    numCheck = True
+                    #**CHECK THIS, returning rowInput to keep the row number in an object.
+                    return(x)
+                    #Should be planning to move out of this function at this point, take the information and move.
+                    #maybe storing in an object for easy scope work?
+                    #move back to driver after you know what row to work in, take information to be remembered.
+                except IndexError:
+                    # Index Out of Bound
+                    #if fail, return loop and start ober 
+                    #UPDATE: Inf loop on failure need to fix
+                    print("value doesnt exist")
+                    numCheck = True
+            ticker += 1
+
+def main():
+    class fileSearch:
+        def __init__(self, columnSearch,):
+            self.columnSearch = columnSearch
+
+    #placeholder name
+    file = fileSearch("")
+
+    #for demo purposes this uses us-area-code-cities.csv
+    print('Please insert file you wish to edit')
+    file.columnSearch = Search()
+    print(file.columnSearch, ' || columnSearch')
+
+    userMenu = '0'
+    while userMenu == '0':
+        print("How do you wish to edit this file?")
+        print("(1.)Add Row")
+        print("(2.)Delete Row")
+        print("(3.)Sort file")
+        print("(4.)Modify column")
+
+        userMenu = input("Menu Input: ")
+        
+        if userMenu == '1'
+            print("Adding row....")
+        if userMenu == '2'
+            print("Deleting row....")
+        if userMenu == '3'
+            print("Sorting row....")
+        if userMenu == '4'
+            print("Modifying row....")
+
+main()
+#then what?
